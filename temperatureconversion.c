f2bd4eaea24afb299cb774a1b0d88fec91489acb
ilovegithub15 <142916985+ilovegithub15@users.noreply.github.com>
2024-10-06 23:10:45 -0500

    added conversion functions

--- temperatureconversion.c
+++ temperatureconversion.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+float celsius_to_fahrenheit(float celsius) {
+    return (9.0 / 5.0) * celsius + 32;
+}
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (5.0 / 9.0) * (fahrenheit - 32);
+}
+float celsius_to_kelvin(float celsius) {
+    return celsius + 273.15;
+}
+float kelvin_to_celsius(float kelvin) {
+    return kelvin - 273.15;
+}
 int main() {
     float temperature;
     int input_scale, target_scale;
